0bf04fae76f82a2aaa7019049072ebbe187787b6
salo322 <salo.sulaberidze7@gmail.com>
2020-05-01 08:06:37 -0700

    update
    update code in popup.html, content.js, popup.js and popup.css. add users length.

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,9 @@
 let value  = $('.bl').html();
 chrome.storage.local.set({friends:  value}, function() {
-console.log('Value is set to ' + value);
-});
 
+}); 
+
+let lengths = $('.bl').children();
+let num = lengths.length;
+console.log(num);
+chrome.storage.local.set({leng: num});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,6 @@
-chrome.storage.local.get(['friends'],function(result) {
+chrome.storage.local.get(['friends','leng'],function(result) {
    $(".ul_list").html(result.friends);
-  
+   $('.userNum').html(result.leng)
   });
 
+  
